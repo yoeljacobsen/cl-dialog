5377e85fa45d08a12acda853caa9007fdd83262d
Yoel Jacobsen <yoel.jacobsen@gmail.com>
2020-02-21 10:54:12 +0200

    middle of buildlist

--- tests.lisp
+++ tests.lisp
@@ -1,6 +1,11 @@
 (in-package #:cl-dialog)
 
-(defun example ()
+(defun test-yesno ()
   (dialog-init)
   (yesno "A Title" "A prompt")
+  (end-dialog))
+
+(defun test-buildlist()
+  (dialog-init)
+  (buildlist "A Title" "A prompt" '(one two three for five))
   (end-dialog))
--- widgets.lisp
+++ widgets.lisp
@@ -1,5 +1,34 @@
 (in-package #:cl-dialog)
 
+;;; dialog_buildlist
+
+(defcfun "dialog_buildlist" :int
+  (title :string)
+  (cprompt :string)
+  (height :int)
+  (width :int)
+  (list-height :int)
+  (item-no :int)
+  (items :pointer)
+  (order-mode :int))
+
+(defun buildlist (title prompt items
+                  &optional
+                    (height 0)
+                    (width 0)
+                    (list-height 0))
+
+  (let* ((string-items (loop for item in items
+                             collect (format nil "~a" item)))
+         (citems (foreign-alloc :string
+                                :initial-contents string-items
+                                :null-terminated-p nil)))
+    (format t "Items: ~a~%" string-items)
+    (dialog-buildlist title prompt height width
+                      list-height (length items) citems 0)))
+
+;;; dialog_yesno
+
 (defcfun "dialog_yesno" :int
   (title :string)
   (cprompt :string)
@@ -8,3 +37,5 @@
 
 (defun yesno (title prompt)
   (dialog-yesno title prompt 0 0))
+
+
